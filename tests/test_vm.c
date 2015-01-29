102bd27bc22599da54d934a9f058e548b6c60156
Dibyendu Majumdar <noemail>
2015-01-29 23:56:20 +0000

    remove  arithmatic operators where constant folding would take care of it

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -576,7 +576,7 @@ int main(const char *argv[])
 {
     int failures = 0;
     failures += test_luacomp1("return 1.5*3");
-    failures += test_luacomp1("local t = {}; local da : double[] = {}; da=t[1];");
+    failures += test_luacomp1("local t = {}; local da : double[] = {}; da=t[1];") == 1 ? 0: 1;
     failures += test_luacompexec1("local function tryme(x); print(#x); return x; end; local da: double[] = { 5, 6 }; da[1] = 42; da = tryme(da); return da[1];", 42);
     /* following should fail as x is a double[] */
     failures += test_luacompexec1("local function tryme(x); print(#x); x[1] = 'junk'; return x; end; local da: double[] = {}; da[1] = 42; da = tryme(da); return da[1];", 42) == 1 ? 0 : 1;
