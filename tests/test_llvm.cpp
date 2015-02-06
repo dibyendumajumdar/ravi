380c37ec900122131bada86c1fd26b35d7d34670
Dibyendu Majumdar <noemail>
2015-02-06 01:08:57 +0000

    first test of LLVM JIT

--- tests/test_llvm.cpp
+++ tests/test_llvm.cpp
@@ -1,18 +1,67 @@
-#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/MCJIT.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+#include "llvm/ExecutionEngine/GenericValue.h"
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/IRBuilder.h"
-#include <vector>
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/ADT/Triple.h"
+
+#include <cstdio>
 #include <string>
+#include <vector>
+#include <memory>
+
+// This mirrors the Lua GCObject structure in lobject.h
+typedef struct RaviGCObject_st {
+  struct RaviGCObject_st *next;
+  unsigned char b1;
+  unsigned char b2;
+} RaviGCObject_t;
+
+// Our prototype for the JITted function
+typedef int (*myfunc_t)(RaviGCObject_t *);
 
 int main() {
+
+  // Looks like unless following three lines are not executed then
+  // ExecutionEngine cannot be created
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
+
   // Get context - need not be global
   llvm::LLVMContext &context = llvm::getGlobalContext();
+
   // Module is the translation unit
-  llvm::Module *module = new llvm::Module("ravi", context);
+  std::unique_ptr<llvm::Module> theModule =
+      std::unique_ptr<llvm::Module>(new llvm::Module("ravi", context));
+  llvm::Module *module = theModule.get();
   llvm::IRBuilder<> builder(context);
+
+#ifdef _WIN32
+  // On Windows we get error saying incompatible object format
+  // Reading posts on mailining lists I found that the issue is that COEFF
+  // format
+  // is not supported and therefore we need to set -elf as the object format
+  module->setTargetTriple("x86_64-pc-win32-elf");
+#endif
+
+  // We will create the following C code
+  //
+  // extern int printf(const char *, ...);
+  //
+  // struct RaviObject {
+  //   struct RaviObject *next;
+  //   unsigned char a;
+  //   unsigned char b;
+  // };
+  //
+  // void testfunc(struct RaviObject *obj) {
+  //   printf("value = %d\n", obj->a);
+  // }
+  //
 
   /* create a GCObject structure as defined in lobject.h */
   llvm::StructType *myt = llvm::StructType::create(context, "RaviGCObject");
@@ -24,18 +73,20 @@ int main() {
   myt->setBody(elements);
   myt->dump();
 
-  // get printf() function
+  // Create printf declaration
   std::vector<llvm::Type *> args;
   args.push_back(llvm::Type::getInt8PtrTy(context));
-  // accepts a char*, is vararg, and returns int 
-  llvm::FunctionType *printfType = llvm::FunctionType::get(builder.getInt32Ty(), args, true);
-  llvm::Constant *printfFunc = module->getOrInsertFunction("printf", printfType);
+  // accepts a char*, is vararg, and returns int
+  llvm::FunctionType *printfType =
+      llvm::FunctionType::get(builder.getInt32Ty(), args, true);
+  llvm::Constant *printfFunc =
+      module->getOrInsertFunction("printf", printfType);
 
   // Create the testfunc()
   args.clear();
   args.push_back(pmyt);
   llvm::FunctionType *funcType =
-      llvm::FunctionType::get(builder.getVoidTy(), args, false);
+      llvm::FunctionType::get(builder.getInt32Ty(), args, false);
   llvm::Function *mainFunc = llvm::Function::Create(
       funcType, llvm::Function::ExternalLinkage, "testfunc", module);
   llvm::BasicBlock *entry =
@@ -52,23 +103,55 @@ int main() {
 
   // Now we need a GEP for the second field in RaviGCObject
   std::vector<llvm::Value *> values;
-  llvm::APInt zero(32, 0);  
+  llvm::APInt zero(32, 0);
   llvm::APInt one(32, 1);
   // This is the array offset into RaviGCObject*
-  values.push_back(llvm::Constant::getIntegerValue(llvm::Type::getInt32Ty(context), zero));
+  values.push_back(
+      llvm::Constant::getIntegerValue(llvm::Type::getInt32Ty(context), zero));
   // This is the field offset
-  values.push_back(llvm::Constant::getIntegerValue(llvm::Type::getInt32Ty(context), one));
+  values.push_back(
+      llvm::Constant::getIntegerValue(llvm::Type::getInt32Ty(context), one));
 
   // Create the GEP value
-  llvm::Value *arg1_a = builder.CreateGEP(arg1, values, "a");
+  llvm::Value *arg1_a = builder.CreateGEP(arg1, values, "ptr");
+
+  // Now retrieve the data from the pointer address
+  llvm::Value *tmp1 = builder.CreateLoad(arg1_a, "a");
+  // As the retrieved value is a byte - convert to int i
+  llvm::Value *tmp2 =
+      builder.CreateZExt(tmp1, llvm::Type::getInt32Ty(context), "i");
 
   // Call the printf function
   values.clear();
   values.push_back(formatStr);
-  values.push_back(arg1_a);
+  values.push_back(tmp2);
   builder.CreateCall(printfFunc, values);
-  builder.CreateRetVoid();
+
+  // return i
+  builder.CreateRet(tmp2);
   module->dump();
 
-  return 0;
+  std::string errStr;
+  auto engine = llvm::EngineBuilder(module)
+                    .setErrorStr(&errStr)
+                    .setEngineKind(llvm::EngineKind::JIT)
+                    .setUseMCJIT(true)
+                    .create();
+  if (!engine) {
+    llvm::errs() << "Failed to construct ExecutionEngine: " << errStr << "\n";
+    return 1;
+  }
+  RaviGCObject_t obj = {0, 42, 65};
+
+  std::string funcname = "testfunc";
+  myfunc_t funcptr = (myfunc_t)engine->getFunctionAddress(funcname);
+  if (funcptr == nullptr) {
+    llvm::errs() << "Failed to obtain compiled function\n";
+    return 1;
+  }
+
+  int ans = funcptr(&obj);
+
+  printf("Answer is %d\n", ans);
+  return ans == 42 ? 0 : 1;
 }
