ec08e257e8ba73f07add12ed0850f82f4682ba95
Dibyendu Majumdar <nomail>
2015-08-13 22:35:07 +0100

    test libgccjit on OSX Yosemite

--- cmake/FindGCCJIT.cmake
+++ cmake/FindGCCJIT.cmake
@@ -1,12 +1,14 @@
 find_path(GCCJIT_INC libgccjit.h
   PATHS
   ~/local/include
+  /usr/local/include
 )
 
 find_library(GCCJIT_LIB
-  NAMES gccjit libgccjit
+  NAMES gccjit libgccjit 
   PATHS
   ~/local/lib
+  /usr/local/lib/gcc/5
 )
 
 set( GCCJIT_INCLUDE_DIRS "${GCCJIT_INC}" )
