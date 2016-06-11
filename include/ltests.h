91bada14cf3bb64e0439c8c1c75ecfb9bb4d244a
Dibyendu Majumdar <noemail>
2016-06-11 15:37:51 +0100

    issue #86: merge Lua 5.3.3

--- include/ltests.h
+++ include/ltests.h
@@ -1,5 +1,5 @@
 /*
-** $Id: ltests.h,v 2.47 2014/12/26 14:44:44 roberto Exp $
+** $Id: ltests.h,v 2.49 2015/09/22 14:18:24 roberto Exp $
 ** Internal Header for Debugging of the Lua Implementation
 ** See Copyright Notice in lua.h
 */
@@ -41,6 +41,15 @@
 
 /* to avoid warnings, and to make sure value is really unused */
 #define UNUSED(x)       (x=0, (void)(x))
+
+
+/* test for sizes in 'l_sprintf' (make sure whole buffer is available) */
+#undef l_sprintf
+#if !defined(LUA_USE_C89)
+#define l_sprintf(s,sz,f,i)	(memset(s,0xAB,sz), snprintf(s,sz,f,i))
+#else
+#define l_sprintf(s,sz,f,i)	(memset(s,0xAB,sz), sprintf(s,f,i))
+#endif
 
 
 /* memory-allocator control variables */
