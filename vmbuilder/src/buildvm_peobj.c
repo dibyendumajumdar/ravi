ca0fbdfd8478495b4c4e46a4f81900871f5e85e2
Dibyendu Majumdar <noemail>
2017-11-14 21:09:45 +0000

    issue #135 understanding peobject

--- vmbuilder/src/buildvm_peobj.c
+++ vmbuilder/src/buildvm_peobj.c
@@ -37,16 +37,39 @@ typedef struct PEheader {
 
 /* PE section. */
 typedef struct PEsection {  
-  char name[8]; /* An 8-byte, null-padded UTF-8 encoded string. If the string is exactly 8 characters long, there is no terminating null. For longer names, this field contains a slash (/) that is followed by an ASCII representation of a decimal number that is an offset into the string table. Executable images do not use a string table and do not support section names longer than 8 characters. Long names in object files are truncated if they are emitted to an executable file. */
-  uint32_t VirtualSize; /* The total size of the section when loaded into memory. If this value is greater than SizeOfRawData, the section is zero-padded. This field is valid only for executable images and should be set to zero for object files. */
-  uint32_t VirtualAddress; /* For executable images, the address of the first byte of the section relative to the image base when the section is loaded into memory. For object files, this field is the address of the first byte before relocation is applied; for simplicity, compilers should set this to zero. Otherwise, it is an arbitrary value that is subtracted from offsets during relocation. */
-  uint32_t SizeOfRawData; /* The size of the section (for object files) or the size of the initialized data on disk (for image files). For executable images, this must be a multiple of FileAlignment from the optional header. If this is less than VirtualSize, the remainder of the section is zero-filled. Because the SizeOfRawData field is rounded but the VirtualSize field is not, it is possible for SizeOfRawData to be greater than VirtualSize as well. When a section contains only uninitialized data, this field should be zero. */
-  uint32_t PointerToRawData; /* The file pointer to the first page of the section within the COFF file. For executable images, this must be a multiple of FileAlignment from the optional header. For object files, the value should be aligned on a 4-byte boundary for best performance. When a section contains only uninitialized data, this field should be zero. */
-  uint32_t PointerToRelocations; /* The file pointer to the beginning of relocation entries for the section. This is set to zero for executable images or if there are no relocations. */
-  uint32_t PointerToLinenumbers; /* The file pointer to the beginning of line-number entries for the section. This is set to zero if there are no COFF line numbers. This value should be zero for an image because COFF debugging information is deprecated. */
-  uint16_t NumberOfRelocations;
-  uint16_t nline;
-  uint32_t flags;
+  char name[8]; /* An 8-byte, null-padded UTF-8 encoded string. If the string is exactly 8 characters long, 
+                   there is no terminating null. For longer names, this field contains a slash (/) that is 
+                   followed by an ASCII representation of a decimal number that is an offset into the string table. 
+                   Executable images do not use a string table and do not support section names longer than 
+                   8 characters. Long names in object files are truncated if they are emitted to an executable file. */
+  uint32_t VirtualSize; /* The total size of the section when loaded into memory. If this value is greater 
+                           than SizeOfRawData, the section is zero-padded. This field is valid only for 
+                           executable images and should be set to zero for object files. */
+  uint32_t VirtualAddress; /* For executable images, the address of the first byte of the section relative 
+                              to the image base when the section is loaded into memory. For object files, 
+                              this field is the address of the first byte before relocation is applied; 
+                              for simplicity, compilers should set this to zero. Otherwise, it is an arbitrary 
+                              value that is subtracted from offsets during relocation. */
+  uint32_t SizeOfRawData; /* The size of the section (for object files) or the size of the initialized 
+                             data on disk (for image files). For executable images, this must be a multiple 
+                             of FileAlignment from the optional header. If this is less than VirtualSize, 
+                             the remainder of the section is zero-filled. Because the SizeOfRawData field 
+                             is rounded but the VirtualSize field is not, it is possible for SizeOfRawData to be 
+                             greater than VirtualSize as well. When a section contains only uninitialized data, 
+                             this field should be zero. */
+  uint32_t PointerToRawData; /* The file pointer to the first page of the section within the COFF file. For 
+                                executable images, this must be a multiple of FileAlignment from the optional header. 
+                                For object files, the value should be aligned on a 4-byte boundary for best performance. 
+                                When a section contains only uninitialized data, this field should be zero. */
+  uint32_t PointerToRelocations; /* The file pointer to the beginning of relocation entries for the section. 
+                                    This is set to zero for executable images or if there are no relocations. */
+  uint32_t PointerToLinenumbers; /* The file pointer to the beginning of line-number entries for the section. This is 
+                                    set to zero if there are no COFF line numbers. This value should be zero for an 
+                                    image because COFF debugging information is deprecated. */
+  uint16_t NumberOfRelocations; /* The number of relocation entries for the section. This is set to zero for executable images. */
+  uint16_t NumberOfLinenumbers; /* The number of line-number entries for the section. This value should be zero for an 
+                                   image because COFF debugging information is deprecated. */
+  uint32_t Characteristics;
 } PEsection;
 
 /* PE relocation. */
@@ -62,16 +85,25 @@ typedef struct PEreloc {
 /* PE symbol table entry. */
 typedef struct PEsym {
   union {
-    char name[8];
+    char ShortName[8]; /* An array of 8 bytes. This array is padded with nulls on the right if the name is less than 8 bytes long. */
     uint32_t nameref[2];
   } n;
-  uint32_t value;
-  int16_t sect;
-  uint16_t type;
-  uint8_t scl;
-  uint8_t naux;
+  uint32_t Value; /* The value that is associated with the symbol. The interpretation of this field depends on SectionNumber and StorageClass. A typical meaning is the relocatable address. */
+  int16_t SectionNumber; /* The signed integer that identifies the section, using a one-based index into the section table. Some values have special meaning */
+  uint16_t Type; /* A number that represents type. Microsoft tools set this field to 0x20 (function) or 0x0 (not a function). For more information */
+  uint8_t StorageClass; /* An enumerated value that represents storage class. */
+  uint8_t NumberOfAuxSymbols; /* The number of auxiliary symbol table entries that follow this record. */
 } PEsym;
-
+/* Cannot use sizeof, because it pads up to the max. alignment. */
+#define PEOBJ_SYM_SIZE  (8+4+2+2+1+1)
+//assert(PEOBJ_SYM_SIZE == 18); // must be 18 bytes
+/*
+Zero or more auxiliary symbol-table records immediately follow each standard symbol-table record. 
+However, typically not more than one auxiliary symbol-table record follows a standard symbol-table record 
+(except for .file records with long file names). Each auxiliary record is the same size as a standard symbol-table 
+record (18 bytes), but rather than define a new symbol, the auxiliary record gives additional information on the
+last symbol defined. The choice of which of several formats to use depends on the StorageClass field.
+*/
 /* PE symbol table auxiliary entry for a section. */
 typedef struct PEsymaux {
   uint32_t size;
@@ -83,42 +115,23 @@ typedef struct PEsymaux {
   uint8_t unused[3];
 } PEsymaux;
 
-/* Cannot use sizeof, because it pads up to the max. alignment. */
-#define PEOBJ_SYM_SIZE	(8+4+2+2+1+1)
+
 
 /* PE object CPU specific defines. */
-#if RAVI_TARGET_X86
-#define PEOBJ_ARCH_TARGET	0x014c
-#define PEOBJ_RELOC_REL32	0x14  /* MS: REL32, GNU: DISP32. */
-#define PEOBJ_RELOC_DIR32	0x06
-#define PEOBJ_RELOC_OFS		0
-#define PEOBJ_TEXT_FLAGS	0x60500020  /* 60=r+x, 50=align16, 20=code. */
-#elif RAVI_TARGET_X64
 #define PEOBJ_ARCH_TARGET	0x8664  /* Image file AMD64 */
 #define PEOBJ_RELOC_REL32	0x04  /* MS: REL32, GNU: DISP32. */
 #define PEOBJ_RELOC_DIR32	0x02
 #define PEOBJ_RELOC_ADDR32NB	0x03
 #define PEOBJ_RELOC_OFS		0
 #define PEOBJ_TEXT_FLAGS	0x60500020  /* 60=r+x, 50=align16, 20=code. */
-#elif RAVI_TARGET_PPC
-#define PEOBJ_ARCH_TARGET	0x01f2
-#define PEOBJ_RELOC_REL32	0x06
-#define PEOBJ_RELOC_DIR32	0x02
-#define PEOBJ_RELOC_OFS		(-4)
-#define PEOBJ_TEXT_FLAGS	0x60400020  /* 60=r+x, 40=align8, 20=code. */
-#endif
 
 /* Section numbers (0-based). */
 enum {
   PEOBJ_SECT_ABS = -2,
   PEOBJ_SECT_UNDEF = -1,
   PEOBJ_SECT_TEXT,
-#if RAVI_TARGET_X64
   PEOBJ_SECT_PDATA,
   PEOBJ_SECT_XDATA,
-#elif RAVI_TARGET_X86
-  PEOBJ_SECT_SXDATA,
-#endif
   PEOBJ_SECT_RDATA_Z,
   PEOBJ_NSECTIONS
 };
@@ -144,8 +157,8 @@ static void emit_peobj_sym(BuildCtx *ctx, const char *name, uint32_t value,
     return;
   }
   if (len <= 8) {
-    memcpy(sym.n.name, name, len);
-    memset(sym.n.name+len, 0, 8-len);
+    memcpy(sym.n.ShortName, name, len);
+    memset(sym.n.ShortName+len, 0, 8-len);
   } else {
     sym.n.nameref[0] = 0;
     sym.n.nameref[1] = (uint32_t)strtabofs;
@@ -153,11 +166,11 @@ static void emit_peobj_sym(BuildCtx *ctx, const char *name, uint32_t value,
     strtab[strtabofs+len] = 0;
     strtabofs += len+1;
   }
-  sym.value = value;
-  sym.sect = (int16_t)(sect+1);  /* 1-based section number. */
-  sym.type = (uint16_t)type;
-  sym.scl = (uint8_t)scl;
-  sym.naux = 0;
+  sym.Value = value;
+  sym.SectionNumber = (int16_t)(sect+1);  /* 1-based section number. */
+  sym.Type = (uint16_t)type;
+  sym.StorageClass = (uint8_t)scl;
+  sym.NumberOfAuxSymbols = 0;
   owrite(ctx, &sym, PEOBJ_SYM_SIZE);
 }
 
@@ -167,12 +180,12 @@ static void emit_peobj_sym_sect(BuildCtx *ctx, PEsection *pesect, int sect)
   PEsym sym;
   PEsymaux aux;
   if (!strtab) return;  /* Pass 1: no output. */
-  memcpy(sym.n.name, pesect[sect].name, 8);
-  sym.value = 0;
-  sym.sect = (int16_t)(sect+1);  /* 1-based section number. */
-  sym.type = PEOBJ_TYPE_NULL;
-  sym.scl = PEOBJ_SCL_STATIC;
-  sym.naux = 1;
+  memcpy(sym.n.ShortName, pesect[sect].name, 8);
+  sym.Value = 0;
+  sym.SectionNumber = (int16_t)(sect+1);  /* 1-based section number. */
+  sym.Type = PEOBJ_TYPE_NULL;
+  sym.StorageClass = PEOBJ_SCL_STATIC;
+  sym.NumberOfAuxSymbols = 1;
   owrite(ctx, &sym, PEOBJ_SYM_SIZE);
   memset(&aux, 0, sizeof(PEsymaux));
   aux.size = pesect[sect].SizeOfRawData;
@@ -191,6 +204,16 @@ void emit_peobj(BuildCtx *ctx)
 
   sofs = sizeof(PEheader) + PEOBJ_NSECTIONS*sizeof(PEsection);
 
+  enum {
+    IMAGE_SCN_CNT_CODE = 0x00000020, /* The section contains executable code. */
+    IMAGE_SCN_CNT_INITIALIZED_DATA = 0x00000040, /* The section contains initialized data. */
+    IMAGE_SCN_LNK_INFO = 0x00000200, /* The section contains comments or other information. The .drectve section has this Type. This is valid for object files only. */
+    IMAGE_SCN_ALIGN_4BYTES = 0x00300000, /* Align data on a 4-byte boundary. Valid only for object files. */
+    IMAGE_SCN_ALIGN_16BYTES = 0x00500000, /* Align data on a 16-byte boundary. Valid only for object files. */
+    IMAGE_SCN_MEM_READ = 0x40000000, /* The section can be read. */
+    IMAGE_SCN_MEM_EXECUTE = 0x20000000, /* The section can be executed as code. */
+  };
+
   /* Fill in PE sections. */
   memset(&pesect, 0, PEOBJ_NSECTIONS*sizeof(PEsection));
   memcpy(pesect[PEOBJ_SECT_TEXT].name, ".text", sizeof(".text")-1);
@@ -199,16 +222,15 @@ void emit_peobj(BuildCtx *ctx)
   pesect[PEOBJ_SECT_TEXT].PointerToRelocations = sofs;
   sofs += (pesect[PEOBJ_SECT_TEXT].NumberOfRelocations = (uint16_t)ctx->nreloc) * PEOBJ_RELOC_SIZE;
   /* Flags: 60 = read+execute, 50 = align16, 20 = code. */
-  pesect[PEOBJ_SECT_TEXT].flags = PEOBJ_TEXT_FLAGS;
+  pesect[PEOBJ_SECT_TEXT].Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_ALIGN_16BYTES | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_EXECUTE; //   ;PEOBJ_TEXT_FLAGS;
 
-#if RAVI_TARGET_X64
   memcpy(pesect[PEOBJ_SECT_PDATA].name, ".pdata", sizeof(".pdata")-1);
   pesect[PEOBJ_SECT_PDATA].PointerToRawData = sofs;
   sofs += (pesect[PEOBJ_SECT_PDATA].SizeOfRawData = 6*4);
   pesect[PEOBJ_SECT_PDATA].PointerToRelocations = sofs;
   sofs += (pesect[PEOBJ_SECT_PDATA].NumberOfRelocations = 6) * PEOBJ_RELOC_SIZE;
   /* Flags: 40 = read, 30 = align4, 40 = initialized data. */
-  pesect[PEOBJ_SECT_PDATA].flags = 0x40300040;
+  pesect[PEOBJ_SECT_PDATA].Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_ALIGN_4BYTES | IMAGE_SCN_MEM_READ; //                0x40300040;
 
   memcpy(pesect[PEOBJ_SECT_XDATA].name, ".xdata", sizeof(".xdata")-1);
   pesect[PEOBJ_SECT_XDATA].PointerToRawData = sofs;
@@ -216,21 +238,13 @@ void emit_peobj(BuildCtx *ctx)
   pesect[PEOBJ_SECT_XDATA].PointerToRelocations = sofs;
   sofs += (pesect[PEOBJ_SECT_XDATA].NumberOfRelocations = 1) * PEOBJ_RELOC_SIZE;
   /* Flags: 40 = read, 30 = align4, 40 = initialized data. */
-  pesect[PEOBJ_SECT_XDATA].flags = 0x40300040;
-#elif RAVI_TARGET_X86
-  memcpy(pesect[PEOBJ_SECT_SXDATA].name, ".sxdata", sizeof(".sxdata")-1);
-  pesect[PEOBJ_SECT_SXDATA].PointerToRawData = sofs;
-  sofs += (pesect[PEOBJ_SECT_SXDATA].SizeOfRawData = 4);
-  pesect[PEOBJ_SECT_SXDATA].PointerToRelocations = sofs;
-  /* Flags: 40 = read, 30 = align4, 02 = lnk_info, 40 = initialized data. */
-  pesect[PEOBJ_SECT_SXDATA].flags = 0x40300240;
-#endif
+  pesect[PEOBJ_SECT_XDATA].Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_ALIGN_4BYTES | IMAGE_SCN_MEM_READ;  // 0x40300040;
 
   memcpy(pesect[PEOBJ_SECT_RDATA_Z].name, ".rdata$Z", sizeof(".rdata$Z")-1);
   pesect[PEOBJ_SECT_RDATA_Z].PointerToRawData = sofs;
   sofs += (pesect[PEOBJ_SECT_RDATA_Z].SizeOfRawData = (uint32_t)strlen(ctx->dasm_ident)+1);
   /* Flags: 40 = read, 30 = align4, 40 = initialized data. */
-  pesect[PEOBJ_SECT_RDATA_Z].flags = 0x40300040;
+  pesect[PEOBJ_SECT_RDATA_Z].Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_ALIGN_4BYTES | IMAGE_SCN_MEM_READ;; // 0x40300040;
 
   /* Fill in PE header. */
   pehdr.Machine = PEOBJ_ARCH_TARGET;
