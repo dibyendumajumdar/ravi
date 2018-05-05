60eccabd2bb902353f57ebc9808771a9b9f75d98
Dibyendu Majumdar <noemail>
2018-05-05 20:04:50 +0100

    issue #151 enable dlmalloc by default

--- src/lauxlib.c
+++ src/lauxlib.c
@@ -1052,9 +1052,9 @@ static int panic (lua_State *L) {
 
 
 LUALIB_API lua_State *luaL_newstate (void) {
-  //void *ud = create_mspace(0, 0);  /* Use Doug Lea's allocator */
-  //lua_State *L = lua_newstate(ravi_alloc_f, ud);
-  lua_State *L = lua_newstate(l_alloc, NULL);
+  void *ud = create_mspace(0, 0);  /* Use Doug Lea's allocator */
+  lua_State *L = lua_newstate(ravi_alloc_f, ud);
+  //lua_State *L = lua_newstate(l_alloc, NULL);
   if (L) lua_atpanic(L, &panic);
   return L;
 }
