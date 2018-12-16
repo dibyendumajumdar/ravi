01eb83dcfe30368633f18161a85414daf3fc407c
Dibyendu Majumdar <mobile@majumdar.org.uk>
2018-12-16 12:41:21 +0000

    issue #161 avoid taking address of math functions in the fast call scenario, instead assign unique codes to functions that may be inlined

--- src/ravijit.cpp
+++ src/ravijit.cpp
@@ -272,9 +272,9 @@ static const luaL_Reg ravilib[] = {{"iscompiled", ravi_is_compiled},
 LUAMOD_API int raviopen_llvmjit(lua_State *L) {
   luaL_newlib(L, ravilib);
   /* faster calls some maths functions */
-  ravi_pushcfastcall(L, (void *)exp, RAVI_TFCF_D_D);
+  ravi_pushcfastcall(L, NULL, RAVI_TFCF_EXP);
   lua_setfield(L, -2, "exp");
-  ravi_pushcfastcall(L, (void *)log, RAVI_TFCF_D_D);
+  ravi_pushcfastcall(L, NULL, RAVI_TFCF_LOG);
   lua_setfield(L, -2, "ln");
   return 1;
 }
