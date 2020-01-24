d32c4189511cba140ea71c317709ab27518cc531
Dibyendu Majumdar <noemail>
2020-01-24 23:19:59 +0000

    issue #182

--- cmake/mir.cmake
+++ cmake/mir.cmake
@@ -19,9 +19,8 @@ set(C2MIR_SRCS
     mir/c2mir/c2mir.c
     )
 
-add_definitions(-D${TARGET})
-add_definitions(-DMIR_IO)
-add_definitions(-DMIR_SCAN)
+set_source_files_properties(${MIR_SRCS} PROPERTIES
+        COMPILE_DEFINITIONS "${TARGET};MIR_IO;MIR_SCAN")
 
 include_directories("mir")
 include_directories("mir/c2mir")
