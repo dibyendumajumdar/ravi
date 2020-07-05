d8a60ddd235733a78ab4c04634c6919753914253
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-07-05 12:02:32 +0100

    issue #163 Split out defer tests to a separate file. Also backport a few tests from Lua 5.4

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -44,6 +44,8 @@ run_lua53_tests "_port=true ravi.auto(true,1)" "Lua53 auto JIT all test failed"
 
 run_ravi_tests language ravi_tests1.ravi "ravi.jit(false)"
 run_ravi_tests language ravi_tests1.ravi "ravi.auto(true,1)"
+#run_ravi_tests language defer_tests.ravi "ravi.jit(false)"
+#run_ravi_tests language defer_tests.ravi "ravi.auto(true,1)"
 run_ravi_tests language ravi_tests3.ravi "ravi.auto(true,1)"
 run_ravi_tests language ravi_errors.ravi "ravi.auto(true,1)"
 run_ravi_tests language basics.lua "ravi.auto(true,1)"
