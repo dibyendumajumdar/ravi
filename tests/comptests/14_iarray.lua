faff7902b3c662a020a549855367edd31cc33f25
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-10-31 14:48:11 +0000

    issue #198 tests for iarray opcodes

---
+++ tests/comptests/14_iarray.lua
@@ -0,0 +1,21 @@
+f = compiler.load([[
+    local arr: integer[] = table.intarray(10)
+    arr[0] = 10
+    arr[4] = 2
+    arr[11] = 6
+    local sum: integer = 0
+    for i=0,11 do
+        print(arr[i])
+        sum = sum + arr[i]
+    end
+    return sum, arr
+]]
+)
+
+assert(f and type(f) == 'function')
+local s,arr = f()
+assert(s == 18)
+assert(arr[0] == 10)
+assert(arr[4] == 2)
+assert(arr[11] == 6)
+assert(#arr == 11)
