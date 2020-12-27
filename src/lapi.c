b717be20e00e2ae9899b5d44c9745aa9c8d44d8e
Dibyendu Majumdar <mobile@majumdar.org.uk>
2020-12-27 11:37:28 +0000

    Fix a missing check

--- src/lapi.c
+++ src/lapi.c
@@ -1688,7 +1688,7 @@ LUA_API void lua_upvaluejoin (lua_State *L, int fidx1, int n1,
   ravitype_t t1, t2;
   UpVal **up1 = getupvalref(L, fidx1, n1, &f1, &t1);
   UpVal **up2 = getupvalref(L, fidx2, n2, NULL, &t2);
-  if (t1 == t2) {
+  if (t1 == t2 && *up1 != *up2) {
     luaC_upvdeccount(L, *up1);
     *up1 = *up2;
     (*up1)->refcount++;
