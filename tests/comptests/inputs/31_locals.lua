63c903941829efceaadd6462202bb604912dbd59
dibyendumajumdar <mobile@majumdar.org.uk>
2021-06-10 16:32:59 +0100

    local assignment test

---
+++ tests/comptests/inputs/31_locals.lua
@@ -0,0 +1,12 @@
+-- Test 7 from language tests
+f = compiler.load([[
+local a, b = {}, 10
+a[b], b = 20, 30
+return a[10], a[30]
+]]
+)
+assert(f and type(f) == 'function')
+local x,y = f()
+assert(x == 20)
+assert(y == nil)
+print 'Ok'
