7b05056d3d32acec83cfd2dd6a674eac3253189a
Dibyendu Majumdar <mobile@majumdar.org.uk>
2021-07-11 23:58:23 +0100

    #198 add checks in test

--- tests/comptests/inputs/40_genericfor.lua
+++ tests/comptests/inputs/40_genericfor.lua
@@ -1,10 +1,17 @@
 f = compiler.load([[
+    local r = {}
     for k,v in pairs(t) do
         print(k,v)
+        r[k] = v
     end
+    return r
 ]]
 )
 assert(f and type(f) == 'function')
 t = { a = 1, b = 2, c = 3 }
-f()
+x = f()
+assert(t.a == x.a)
+assert(t.b == x.b)
+assert(t.c == x.c)
+
 print 'Ok'
