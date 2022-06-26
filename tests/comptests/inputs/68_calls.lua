0b142eb42db8d94e56d37e5820faedda15df92ac
dibyendumajumdar <mobile@majumdar.org.uk>
2022-06-26 14:52:36 +0100

    #245 more tests

--- tests/comptests/inputs/68_calls.lua
+++ tests/comptests/inputs/68_calls.lua
@@ -5,29 +5,29 @@ function unlpack (t, i)
   end
 end
 
--- function equaltab (t1, t2)
---   assert(#t1 == #t2)
---   local n: integer = #t1
---   for i = 1, n do
---     assert(t1[i] == t2[i])
---   end
--- end
---
--- local pack = table.pack
---
--- function f() return 1,2,30,4 end
--- function ret2 (a,b) return a,b end
+function equaltab (t1, t2)
+  assert(#t1 == #t2)
+  local n: integer = #t1
+  for i = 1, n do
+    assert(t1[i] == t2[i])
+  end
+end
+
+local pack = table.pack
+
+function f() return 1,2,30,4 end
+function ret2 (a,b) return a,b end
 
 local a,b,c,d = unlpack{1,2,3}
 --print(a,b,c,d)
 assert(a==1 and b==2 and c==3 and d==nil)
--- a = {1,2,3,4,false,10,'alo',false,assert}
--- equaltab(pack(unlpack(a)), a)
--- equaltab(pack(unlpack(a), -1), {1,-1})
--- a,b,c,d = ret2(f()), ret2(f())
--- assert(a==1 and b==1 and c==2 and d==nil)
--- a,b,c,d = unlpack(pack(ret2(f()), ret2(f())))
--- assert(a==1 and b==1 and c==2 and d==nil)
+a = {1,2,3,4,false,10,'alo',false,assert}
+equaltab(pack(unlpack(a)), a)
+equaltab(pack(unlpack(a), -1), {1,-1})
+a,b,c,d = ret2(f()), ret2(f())
+assert(a==1 and b==1 and c==2 and d==nil)
+a,b,c,d = unlpack(pack(ret2(f()), ret2(f())))
+assert(a==1 and b==1 and c==2 and d==nil)
 -- a,b,c,d = unlpack(pack(ret2(f()), (ret2(f()))))
 -- assert(a==1 and b==1 and c==nil and d==nil)
 --
